d735e0a5bd2db6bc68cbba53b78ee33dcdc945ce
romuloabbiati <rhabbiati@hotmail.com>
2022-12-31 04:55:50 +0000

    first commit

---
+++ backend/src/main/java/com/smartgroup/smartsearch/SmartsearchApplication.java
@@ -0,0 +1,13 @@
+package com.smartgroup.smartsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartsearchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartgroup/smartsearch/SmartsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgroup.smartsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
